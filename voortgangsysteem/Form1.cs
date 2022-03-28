c617f653b93e6a94931edbe2256b3b031a6a0625
Yasser30007 <234880@rocfriesepoort.nl>
2022-03-28 16:06:29 +0200

    Home pagina

--- voortgangsysteem/Form1.cs
+++ voortgangsysteem/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,38 @@ namespace voortgangsysteem
         private void projectToevoegenToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void projectenoverzichtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 Projectenoverzicht = new Form3();
+            Projectenoverzicht.Show();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void projectenoverzichtToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 Projectenoverzicht = new Form3();
+            Projectenoverzicht.Show();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filebath = @"C:\Users\yasse\OneDrive\Documents\GitHub\voortgangsmetingsysteem\voortgangsysteem\Projecten.txt";
+            List<string> Projecten = new List<string>();
+            File.ReadAllLines(filebath).ToList();
         }
     }
 }
