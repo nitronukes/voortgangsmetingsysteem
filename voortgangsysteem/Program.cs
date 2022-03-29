752a21698de87dc39f4da83512abcff7da680224
Christianfk2905 <c.f.koopman2905@gmail.com>
2022-03-29 14:20:51 +0200

    ff

--- voortgangsysteem/Program.cs
+++ voortgangsysteem/Program.cs
@@ -17,7 +17,7 @@ namespace voortgangsysteem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            Application.Run(new Project_overzichtpagina());
+            Application.Run(new menu());
 
         }
     }
